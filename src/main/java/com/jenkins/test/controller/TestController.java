c0024b1673d99259af17d47989cd4b18e7f18465
op271 <op271@fif.co.id>
2025-10-13 00:58:08 +0700

    Update SpringbootFile

---
+++ src/main/java/com/jenkins/test/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.jenkins.test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jenkins.test.service.TestService;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    
+    @Autowired
+    private TestService testService;
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        String result = testService.test();
+        return ResponseEntity.ok(result);
+    }
+    
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Application is running!");
+    }
+
+}
---
+++ src/main/java/com/jenkins/test/service/TestService.java
@@ -0,0 +1,15 @@
+package com.jenkins.test.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class TestService {
+    
+    public String test() {
+        log.info("TestService test method called");
+        return "Service is working!";
+    }
+}
